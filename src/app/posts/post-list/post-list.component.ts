47e8bd25edf4848bc3c89fe644e72bfaab813a15
felipealencar <felipealencardev@gmail.com>
2018-07-28 15:28:35 -0300

    structural directives

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -6,11 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit {
-  posts = [
-    { tittle: 'First Post', content: 'This is the first post' },
-    { tittle: 'Second Post', content: 'This is the second post' },
-    { tittle: 'Third Post', content: 'This is the third post' }
-  ];
+  // posts = [
+  //   { tittle: 'First Post', content: 'This is the first post' },
+  //   { tittle: 'Second Post', content: 'This is the second post' },
+  //   { tittle: 'Third Post', content: 'This is the third post' }
+  // ];
+
+  posts = [];
 
   constructor() { }
 
