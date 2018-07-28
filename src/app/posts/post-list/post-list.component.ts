08eef28701850211821432da03473fb063aa6c07
felipealencar <felipealencardev@gmail.com>
2018-07-28 15:14:55 -0300

    outputting posts

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+  posts = [
+    { tittle: 'First Post', content: 'This is the first post' },
+    { tittle: 'Second Post', content: 'This is the second post' },
+    { tittle: 'Third Post', content: 'This is the third post' }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
