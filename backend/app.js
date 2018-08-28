e492e077fd192e0d4f03de0d84cf2cf6255cba53
felipealencar <felipealencardev@gmail.com>
2018-08-28 19:54:46 -0300

    adding the POST backend point

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   res.setHeader(
@@ -16,6 +20,14 @@ app.use((req, res, next) => {
     'GET, POST, PATCH, DELETE, OPTIONS'
   );
   next();
+});
+
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post addes successfully'
+  });
 });
 
 app.use('/api/posts', (req, res, next) => {
